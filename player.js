4aed79b22d0a85c83584c11781d9c2b4622dbf4b
EnderYT6222 <yigitsirvanyt@gmail.com>
2025-06-20 20:18:08 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,30 +1,22 @@
-// player.js
 export class Player {
-  constructor(x, y, width = 40, height = 60, color = '#0f0') {
+  constructor(x, y, spritePath) {
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-
+    this.width = 27;
+    this.height = 31;
     this.speed = 3;
     this.vx = 0;
     this.vy = 0;
+    this.sprite = new Image();
+    this.sprite.src = spritePath;
   }
-
   update() {
     this.x += this.vx;
     this.y += this.vy;
-
-    // Ekran sınırları kontrolü
-    if (this.x < 0) this.x = 0;
-    if (this.y < 0) this.y = 0;
-    if (this.x + this.width > 800) this.x = 800 - this.width;
-    if (this.y + this.height > 450) this.y = 450 - this.height;
+    this.x = Math.max(0, Math.min(800 - this.width, this.x));
+    this.y = Math.max(0, Math.min(450 - this.height, this.y));
   }
-
   draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
   }
 }
